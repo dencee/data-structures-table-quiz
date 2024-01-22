820d0d13705d8f515dff761890aa4cf99a9aa92a
daniel commins <daniel.commins@techelevator.com>
2024-01-22 01:17:02 -0800

    typo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/data-structures-table-quiz',
+  base: '/data-structures-table-quiz/',
   plugins: [
     vue(),
   ],
